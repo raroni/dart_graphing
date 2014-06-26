d90f2ff7b8c7892d604e00fab4e2755946e38784
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2014-06-26 23:41:25 +0200

    Removed destinationEdgeLists. No longer any use for these.

--- lib/src/graph.dart
+++ lib/src/graph.dart
@@ -3,7 +3,6 @@ part of graphing;
 class Graph<NodeType extends Node> {
   List<NodeType> nodes = new List<NodeType>();
   List<List<Edge>> _originEdgeLists = new List<List<Edge>>();
-  List<List<Edge>> _destinationEdgeLists = new List<List<Edge>>();
   int nextUnusedIndex = 0;
   List<int> freedIndices = new List<int>();
   
@@ -12,26 +11,16 @@ class Graph<NodeType extends Node> {
       node.index = nextUnusedIndex++;
       nodes.add(node);
       _originEdgeLists.add(new List<Edge>());
-      _destinationEdgeLists.add(new List<Edge>());
     } else {
       node.index = freedIndices.removeLast();
       nodes[node.index] = node;
-      _originEdgeLists[node.index] = new List<Edge>(); 
-      _destinationEdgeLists[node.index] = new List<Edge>(); 
+      _originEdgeLists[node.index] = new List<Edge>();  
     }
   }
   
   void removeNode(node) {
     nodes[node.index] = null;
-    for(var edge in _originEdgeLists[node.index]) {
-      _destinationEdgeLists[edge.destinationNodeIndex].remove(edge);
-    }
     _originEdgeLists[node.index] = null;
-    
-    for(var edge in _destinationEdgeLists[node.index]) {
-      _originEdgeLists[edge.originNodeIndex].remove(edge);
-    }
-    _destinationEdgeLists[node.index] = null;
     freedIndices.add(node.index);
   }
   
@@ -39,7 +28,10 @@ class Graph<NodeType extends Node> {
   
   void addEdge(Edge edge) {
     _originEdgeLists[edge.originNodeIndex].add(edge);
-    _destinationEdgeLists[edge.destinationNodeIndex].add(edge);
+  }
+  
+  void removeEdge(Edge edge) {
+    _originEdgeLists[edge.originNodeIndex].remove(edge);    
   }
   
   List<Edge> getEdgesByNodeIndex(int nodeIndex) {
